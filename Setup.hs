5917288874f7d00913d99bc6edd965c96c53fc88
Calvin Smith <cs-haskell@protempore.net>
2007-10-13 01:50:44 +0000

    Setup.hs: added hook for running tests.

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,13 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription (PackageDescription)
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo, buildDir)
+import System.Exit
+import Control.Monad
+import System.Process
 
+
+main = defaultMainWithHooks defaultUserHooks { runTests = test }
+
+test :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ExitCode
+test _ _ _ _ = waitForProcess =<< runCommand commandStr
+  where commandStr = "./quickcheck +names Denominate_Test.hs"
