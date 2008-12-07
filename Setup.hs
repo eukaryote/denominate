8a94af7470f804d61ac35797d273655993cafcc3
Calvin Smith <cs-haskell@protempore.net>
2008-12-07 23:10:48 +0000

    Some changes to testrunning so that it programmatically determines which base version to use and passes that as an option to the haskell command that runs the tests.

--- Setup.hs
+++ Setup.hs
@@ -10,4 +10,4 @@ main = defaultMainWithHooks (simpleUserHooks { runTests = test })
 
 test :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
 test _ _ _ _ = runCommand commandStr >>= waitForProcess >> return ()
-  where commandStr = "./quickcheck +names System/Denominate_Test.hs"
+  where commandStr = "runhaskell TestRunner +names System/Denominate_Test.hs"
--- TestRunner.hs
+++ TestRunner.hs
@@ -1,4 +1,5 @@
-#!/usr/bin/runhugs
+module TestRunner where
+
 -- 	$Id: quickcheck,v 1.4 2003/01/08 15:09:22 shae Exp $	
 -- This file defines a command
 --      quickCheck <options> <files>
@@ -18,7 +19,13 @@
 -- add a verbosity switch that uses verboseCheck instead of quickCheck
 
 import System
-import List
+import Data.List
+
+-- CS added for getting compiler version for base-? hackery
+import System.Info
+import Data.Version
+import System.Process
+import System.IO
 
 main :: IO ()
 main = do as<-getArgs
@@ -36,13 +43,14 @@ process opts file =
 	                (map (fst.head.lex.unlit) (lines xs)) 
           if null names then
 	      putStr (file++": no properties to check\n")
-	    else do writeFile "hugsin"$
+	    else do baseStr <- ghcBaseVersion
+                    writeFile "hugsin"$
 	              unlines ((":l "++file):
 	                       [(if namesOpt=="+names" then 
 			           "putStr \""++p++": \" >> "
 				 else "") ++
-				"Test.QuickCheck.quickCheckWith 500 5000 100 "++p | p<-names])
-	            system ("ghci -v0 "++options opts'++" <hugsin")
+				"Test.QuickCheck.quickCheckWith (stdArgs { maxSuccess = 500, maxDiscard = 5000, maxSize = 100}) "++p | p<-names])
+	            system ("ghci -v0 -package " ++ baseStr ++ " " ++ options opts' ++ " <hugsin")
 	            return ()
 
 isOption xs = head xs `elem` "-+"
@@ -52,3 +60,17 @@ options opts = unwords ["\""++opt++"\"" | opt<-opts]
 getOption name def opts = 
   let opt = head [opt | opt<-opts++[def], isPrefixOf name (drop 1 opt)] in
     (opt, filter (/=opt) opts)
+
+
+ghcBaseVersion = 
+  do (stdin, stdout, stderr, phandle) <- runInteractiveProcess "ghc-pkg" ["--simple-output", "list", "base"] Nothing Nothing
+     str <- hGetContents stdout -- returns a string like "base-3.0.3.0 base-4.0.0.0\n"
+     -- we use any base-3 version available at present, and if none is available, 
+     -- choose the first base in the list, which should be the earliest.
+     let versions = words $ init str
+     return $ maybe (head versions) id (findBase3Version versions)
+  where
+    findBase3Version []     = Nothing
+    findBase3Version (v:vs) = if "base-3" `isPrefixOf` v
+                                 then Just v
+                                 else findBase3Version vs
