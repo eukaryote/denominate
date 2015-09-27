4ebf79a3f183abcc1ba399d9a8fbe671fdbc2c99
Calvin Smith <cs-haskell@protempore.net>
2015-09-27 15:45:49 -0700

    switched to tasty for testing

--- TestMain.hs
+++ TestMain.hs
@@ -1,12 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-
-import Test.HUnit
-import Test.Framework
-import Test.Framework.Providers.HUnit
-import Test.Framework.Providers.QuickCheck2
-import Test.Framework.TH
+import Test.Tasty
+import Test.Tasty.TH (defaultMainGenerator)
+--import Test.Tasty.SmallCheck as SC
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
 
 import Test.QuickCheck.Arbitrary (Arbitrary)
 import Test.QuickCheck.Property (forAll, Property, (==>))
